9869168281563799514c3ebee6160efdb4d67da0
KaushalRana1999 <142668839+KaushalRana1999@users.noreply.github.com>
2026-05-29 08:12:02 +0530

    Add Vercel SPA routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -8,13 +7,10 @@ import { AuthProvider } from './context/AuthContext';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Provider>
 );
