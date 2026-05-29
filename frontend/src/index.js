a33be883d21b2fc8535754074311bc2c5770ab7f
KaushalRana1999 <142668839+KaushalRana1999@users.noreply.github.com>
2026-05-29 08:08:23 +0530

    Fix React Router and Vercel routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -7,10 +8,13 @@ import { AuthProvider } from './context/AuthContext';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <Provider store={store}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
+  </BrowserRouter>
 );
